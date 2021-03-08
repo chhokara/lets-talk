f06f31384abf0824cbe2af32ce934542894a0e4f
Arshdeep <aschhoka@sfu.ca>
2021-03-07 19:53:37 -0800

    setup server socket

--- lets_talk.c
+++ lets_talk.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include "list.c"
+#include "list.h"
+
+void *receive_msg(void * list) {
+    int sockfd;
+    char buffer[1024];
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(8080); 
+      
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    int len, n; 
+  
+    len = sizeof(cliaddr);  //len is value/resuslt 
+  
+    n = recvfrom(sockfd, (char *)buffer, 1024,  
+                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+
+    List_add(*(List**)list, (char *)buffer);
+    return 0;
+}
+void *print_msg(void * list) {
+    return 0;
+}
 
 int main (int argc, char *argv[]) 
 {
-    printf("hello world\n");
+    List * msg_list;
+    msg_list = List_create();
+
+    pthread_t receiving_thr, printer_thr;
+
+    // create threads for receiving and printing messages
+    pthread_create(&receiving_thr, NULL, receive_msg, &msg_list);
+    pthread_create(&printer_thr, NULL, print_msg, &msg_list);
+
+    pthread_join(receiving_thr, NULL);
+    pthread_join(printer_thr, NULL);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 # build an executable named lets_talk from lets_talk.c
 all: lets_talk.c
-	gcc -g -Wall -o lets_talk lets_talk.c
+	gcc -g -Wall -pthread -o lets_talk lets_talk.c
 
 clean:
 	$(RM) lets_talk
