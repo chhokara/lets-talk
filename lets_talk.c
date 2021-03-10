24927ee6951ab170cdca3cf712a40ef65b3a112e
Arshdeep <aschhoka@sfu.ca>
2021-03-09 23:37:18 -0800

    create threads

--- lets_talk.c
+++ lets_talk.c
@@ -10,61 +10,105 @@
 #include "list.c"
 #include "list.h"
 
-void *receive_msg(void * list) {
-    int sockfd;
-    char buffer[1024];
-    struct sockaddr_in servaddr, cliaddr;
 
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
+char buffer[1000];
+struct thread_params {
+    List * sending_list;
+    List * receiving_list;
+    int argc;
+    char ** argv;
+};
 
-    // Filling server information 
-    servaddr.sin_family    = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(8080); 
+void *receive_msg(void * ptr) {
+    // struct thread_params *params = ptr;
+    // printf("%d\n", List_count(params->receiving_list));
+    // printf("%d\n", atoi(params->argv[1]));
+    // int sockfd;
+    // char buffer[1024];
+    // struct sockaddr_in servaddr, cliaddr;
+
+    // // Creating socket file descriptor 
+    // if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+    //     perror("socket creation failed"); 
+    //     exit(EXIT_FAILURE); 
+    // } 
       
-    // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
-            sizeof(servaddr)) < 0 ) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+    // memset(&servaddr, 0, sizeof(servaddr)); 
+    // memset(&cliaddr, 0, sizeof(cliaddr)); 
+
+    // // Filling server information 
+    // servaddr.sin_family    = AF_INET; // IPv4 
+    // servaddr.sin_addr.s_addr = INADDR_ANY; 
+    // servaddr.sin_port = htons(atoi(params->argv[1])); 
       
-    int len, n; 
+    // // Bind the socket with the server address 
+    // if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+    //         sizeof(servaddr)) < 0 ) 
+    // { 
+    //     perror("bind failed"); 
+    //     exit(EXIT_FAILURE); 
+    // } 
+      
+    // int len, n; 
   
-    len = sizeof(cliaddr);  //len is value/resuslt 
+    // len = sizeof(cliaddr);  //len is value/resuslt 
   
-    n = recvfrom(sockfd, (char *)buffer, 1024,  
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-                &len); 
-    buffer[n] = '\0'; 
+    // n = recvfrom(sockfd, (char *)buffer, 1024,  
+    //             MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+    //             &len); 
+    // buffer[n] = '\0'; 
 
-    List_add(*(List**)list, (char *)buffer);
-    return 0;
+    // List_add(params->receiving_list, (char *)buffer);
+    printf("receive\n");
 }
-void *print_msg(void * list) {
-    return 0;
+void *print_msg(void * ptr) {
+    printf("print\n");
+}
+void *send_msg(void * ptr) {
+    struct thread_params *params = ptr;
+    do {
+        char * msg = List_remove(params->sending_list);
+        printf("%s", msg);
+    } while(true);
+    
+}
+void *input_msg(void * ptr) {
+    struct thread_params *params = ptr;
+    printf("Welcome to lets-talk! Please type your message now\n");
+    do {
+        if(fgets(buffer, 1000, stdin)){
+            List_add(params->sending_list, (char *)buffer);
+        }
+    } while(true);
 }
 
-int main (int argc, char *argv[]) 
+int main (int argc, char ** argv) 
 {
-    List * msg_list;
-    msg_list = List_create();
+    List * send_list;
+    send_list = List_create();
+    
+    List * receive_list;
+    receive_list = List_create();
 
-    pthread_t receiving_thr, printer_thr;
 
-    // create threads for receiving and printing messages
-    pthread_create(&receiving_thr, NULL, receive_msg, &msg_list);
-    pthread_create(&printer_thr, NULL, print_msg, &msg_list);
+    struct thread_params params;
+    params.argc = argc;
+    params.argv = argv;
+    params.sending_list = send_list;
+    params.receiving_list = receive_list;
 
+    pthread_t receiving_thr, printer_thr, sending_thr, keyboard_thr;
+
+    //create threads for receiving and printing messages
+    pthread_create(&keyboard_thr, NULL, input_msg, &params);
+    pthread_create(&sending_thr, NULL, send_msg, &params);
+    pthread_create(&receiving_thr, NULL, receive_msg, &params);
+    pthread_create(&printer_thr, NULL, print_msg, &params);
+
+    pthread_join(keyboard_thr, NULL);
+    pthread_join(sending_thr, NULL);
     pthread_join(receiving_thr, NULL);
     pthread_join(printer_thr, NULL);
+
     return 0;
 }
